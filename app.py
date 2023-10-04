b8d5e20f011479168c84d030af61b355ea73a5ed
Darshan Soni <sonidarshan200@gmail.com>
2023-10-04 23:59:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, url_for, redirect
+import base64
+app = Flask(__name__)
+
+def base64_decode(cipher):
+   decoded_bytes = base64.b64decode(cipher)
+   decoded_string = decoded_bytes.decode('utf-8')
+   return decoded_string
+   
+@app.route('/', methods=['GET','POST'])
+def HomePage():
+    if request.method == "POST":
+        text = request.form["text"]
+        if 'encrypt' in request.form:
+            output = base64.b64encode(text.encode("utf-8"))
+        elif 'decrypt' in request.form:
+            output = base64_decode(text)
+        else:
+            output = "Invalid request"
+
+        return render_template("index.html", output=output)
+    else:
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
