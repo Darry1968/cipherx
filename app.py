9daacd1b72430c4f7951e24f981194fe7578963b
Darshan Soni <sonidarshan200@gmail.com>
2023-10-05 10:50:24 +0530

    Base64 working done

--- app.py
+++ app.py
@@ -7,24 +7,33 @@ def base64_decode(cipher):
    decoded_string = decoded_bytes.decode('utf-8')
    return decoded_string
 
+def base64_encode(text):
+    encoded_bytes = base64.b64encode(text.encode('utf-8'))
+    encoded_string = encoded_bytes.decode('utf-8')
+    return encoded_string
+
 @app.route('/')
 def HomePage():
     return render_template("index.html")
 
-@app.route('/encode', methods=['GET','POST'])
-def encode():
+@app.route('/base64_',methods=['GET','POST'])
+def base64_():
     if request.method == "POST":
         text = request.form["text"]
         if 'encrypt' in request.form:
-            output = base64.b64encode(text.encode("utf-8"))
+            output = base64_encode(text)
         elif 'decrypt' in request.form:
             output = base64_decode(text)
         else:
             output = "Invalid request"
 
-        return render_template("base64.html", output=output)
+        return render_template("base64_.html", output=output)
     else:
-        return render_template("base64.html")
+        return render_template("base64_.html")
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
